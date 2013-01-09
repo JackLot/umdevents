41ff01a30744302981af13dc191dfcc775a4a2f1
Jack Lot <jacklotkowski@gmail.com>
2013-01-09 17:25:54 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Umdevents::Application.config.secret_token = 'e7fa6546ba95e779653d38962ab02144d02704a8b7cbcb8fb54d3b856e46bb20db1549d466ab9c30bd32e96e073a082a22ac674f400c3e953508e828eddaf603'
