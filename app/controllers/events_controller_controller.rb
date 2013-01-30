2524e77de8fd52eec9ea8880cf81f4455ed41616
JackLot <jacklotkowski@gmail.com>
2013-01-30 02:03:05 -0500

    Added ICalendar gem. Created basic reading/writing events page

--- app/controllers/events_controller_controller.rb
+++ app/controllers/events_controller_controller.rb
@@ -5,6 +5,26 @@ class EventsControllerController < ApplicationController
 	end
 	
 	def index
+
+		# Read the mens basketball schedule
+		require 'open-uri'
+		require 'net/http'
+		uri = URI('http://www.umterps.com/ot/2012_MD_MBB.ics')
+		@file = Net::HTTP.get(uri).to_s
+		@cal = Icalendar.parse(@file)
+
+		@firstEvent = @cal.first.events.first
+
+		#This works but on every refresh it creates a new event instance. Also if any field
+		#is incorrect then the whole event doesnt insert
+		Event.create(
+			:name => @firstEvent.summary, 
+			:description => "Mens Basketball", 
+			:start_time => @firstEvent.dtstart, 
+			:end_time => @firstEvent.dtend, 
+			:organization => "UMD Athletics")
+
+
 	end
 
   	def show
